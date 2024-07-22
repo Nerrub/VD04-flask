451133ac1bf1a6ff689038d4538cf366eda65eb6
Nerrub <Lyapin_dmitriy@hotmail.com>
2024-07-22 18:41:17 +0300

    1

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return current_time
+
+if __name__ == '__main__':
+    app.run(debug=True)
